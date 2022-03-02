5345292f5f80026e6efe7e78230c33e37cf4e859
ishifoev <alex.malikov94@gmail.com>
2022-03-02 10:05:39 +0500

    Billing Account

--- src/Google/Google.php
+++ src/Google/Google.php
@@ -9,21 +9,25 @@ class Google {
     public function getCloudBillingInfo($url) {
     putenv('GOOGLE_APPLICATION_CREDENTIALS='.realpath("service-account-file.json"));
     $client = new CloudBillingClient();
-
+    $myArray = [];
     switch($url) {
         case '/google/billingAccounts':
-            $accounts = $client->listBillingAccounts();
-          
-            foreach ($accounts as $account) {
-                print('Billing Accounts: ' . $account->getName() . PHP_EOL);
-            }
+            $response = $client->getBillingAccount("billingAccounts/01C33A-8DD35D-FEEBB5");
+            array_push($myArray, (object)[
+                'billing_name' => $response->getName(),
+                'display_billing_name' => $response->getDisplayName(),
+                'billing_ouput' => $response->getOpen(),
+            ]);
+            echo '<pre>';
+            var_dump($myArray);
+            echo '</pre>';      
+            file_put_contents('google_billing_accounts.json', json_encode($myArray, JSON_PRETTY_PRINT));
         break;
         case '/google/projects': 
             $response = $client->getProjectBillingInfo('projects/vast-descent-341509');
-            $myArray = [];
 
             array_push($myArray, (object)[
-                'name' => $response->getName(),
+                'project_name' => $response->getName(),
                 'project_id' => $response->getProjectId(),
                 'billing_account_name' => $response->getBillingAccountName(),
             ]);

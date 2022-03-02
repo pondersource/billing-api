58e2717898229f9b58cae58fd4d64fe732404308
Triantafullenia-Doumani <triantafulleniad@gmail.com>
2022-03-02 15:57:56 +0100

    implement github client

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use PonderSource\GoogleApi\Google;
 use PonderSource\HerokuApi\HerokuApiEndpoint;
+use PonderSource\GitHubApi\GitHubClient;
 
 $uri = $_SERVER['REQUEST_URI'];
 
@@ -13,5 +14,6 @@ $google->getCloudBillingInfo($uri);
 $her = new HerokuApiEndpoint;
 $her->getUrlAccount($uri);
 
+$github = new GitHubClient();
+$github->getOrgBillingInfo("testORGbilling");
 ?>
-

d92e4c6d98326dae922592e63c7b28327c994533
ishifoev <alex.malikov94@gmail.com>
2022-03-10 16:06:25 +0500

    Update

--- index.php
+++ index.php
@@ -14,22 +14,5 @@ $uri = $_SERVER['REQUEST_URI'];
 
 $her = new HerokuApiEndpoint;
 $her->getHerokuInvoice();
-<<<<<<< HEAD
-=======
-// $google = new Google([
-//     'apiKey' => putenv('GOOGLE_APPLICATION_CREDENTIALS='.realpath("service-account-file.json"))
-// ]);
-// var_dump($google->getCloudbillingSkus());
-//
-// $her = new HerokuApiEndpoint;
-// $her->getUrlAccount($uri);
-//
-// $github = new GitHubClient();
-// $github->getOrgBillingInfo("testORGbilling");
-
-$key = getenv('AWS_ACCESS_KEY_ID');
-$secret = getenv('AWS_SECRET_ACCESS_KEY');
->>>>>>> 3b7b5b53b1b3bb783683c4822fe7b38e34c19259
-
 ?>
 
